3f1b2862206e942069c578b6f28aa2581f812e03
JovanPapi <jovan.gppapi@hotmail.com>
2019-07-02 00:34:22 +0200

    Added ImageBox class & ListImages class

---
+++ BrainyOgrasm/ImageBox.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainyOgrasm
+{
+    public class ImageBox
+    {
+        public Point ImageLocation { get; set; }
+        public Image OriginalImage { get; set; }
+        public const int ImageWidth = 30;
+        public const int ImageHeight = 30;
+
+        public ImageBox(Point imageLocation, String Path)
+        {
+            OriginalImage = Image.FromFile(Path);
+            ImageLocation = imageLocation;
+        }
+
+        public void DrawImage(Graphics g)
+        {
+            g.DrawImage(OriginalImage, ImageLocation.X, ImageLocation.Y, ImageWidth, ImageHeight);
+        }
+
+        public void MoveImageLocation()
+        {
+            this.ImageLocation = new Point(ImageLocation.X, ImageLocation.Y + 30);
+        }
+    }
+}
---
+++ BrainyOgrasm/ListImages.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainyOgrasm
+{
+    public class ListImages
+    {
+        public List<ImageBox> Images { get; set; }
+
+        public ListImages()
+        {
+            this.Images = new List<ImageBox>();
+        }
+
+        public void DrawImages(Graphics g)
+        {
+            foreach (ImageBox im in Images)
+            {
+                im.DrawImage(g);
+            }
+        }
+
+        public void MoveImages(int panelHeight)
+        {
+            for (int i = 0; i < Images.Count; i++)
+            {
+                if (Images[i].ImageLocation.Y > panelHeight + 60)
+                {
+                    Images.Remove(Images[i]); 
+                }
+                else
+                    Images[i].MoveImageLocation();
+            }
+        }
+
+        public void AddImage(ImageBox newImage)
+        {
+            Images.Add(newImage);
+        }
+    }
+}
